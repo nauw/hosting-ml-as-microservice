d21fbb5b412f2af5130dcb7726bd9e2893875e26
Arnaud Charlier <Arnaud>
2020-10-06 22:17:38 +0200

    Make it Pipelined

---
+++ review-ml-api/functions/status.py
@@ -0,0 +1,21 @@
+import json
+
+print('Loading function')
+
+
+def main(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+    
+    return response
+
+
+if __name__ == "__main__":
+    main('','')
